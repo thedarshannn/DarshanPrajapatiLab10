99233c8106794b3f2f992d2a2f55eb8ae4f65bad
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-19 19:48:38 -0400

    Initial Commit.

---
+++ app/src/main/java/darshan/prajapati/n01584247/dp/MainActivity.kt
@@ -0,0 +1,11 @@
+package darshan.prajapati.n01584247.dp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
