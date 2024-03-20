ba3dc952c43d6396f57114c2e22afc9dcfa796b6
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-19 20:49:21 -0400

    Implemented Splash screen.

---
+++ DarshanModule10/src/main/java/darshan/prajapati/n01584247/dp/DarshanPraSplash.kt
@@ -0,0 +1,27 @@
+package darshan.prajapati.n01584247.dp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
+
+class DarshanPraSplash : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000) // Delay for 3 seconds
+
+            // Start your app main activity
+            val i = Intent(this@DarshanPraSplash, PrajapatiActivity10::class.java)
+            startActivity(i)
+
+            // Close this activity
+            finish()
+        }
+    }
+
+}
