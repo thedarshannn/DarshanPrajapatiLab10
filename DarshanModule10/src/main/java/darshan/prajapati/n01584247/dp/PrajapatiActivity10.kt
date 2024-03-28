1f243e3b5f4c26200cd2ca10e18872a4e803e7db
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-28 18:46:02 -0400

    Implemented bottom navigation bar.

--- DarshanModule10/src/main/java/darshan/prajapati/n01584247/dp/PrajapatiActivity10.kt
+++ DarshanModule10/src/main/java/darshan/prajapati/n01584247/dp/PrajapatiActivity10.kt
@@ -1,11 +1,50 @@
 package darshan.prajapati.n01584247.dp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PrajapatiActivity10 : AppCompatActivity() {
+
+    private var bnv: BottomNavigationView? = null
+    private val fragmentList: ArrayList<Fragment> = ArrayList<Fragment>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add fragments to the list
+        fragmentList.add(Da1rshanFragment())
+        fragmentList.add(Pr2ajapatiFragment())
+        fragmentList.add(N013584247Fragment())
+        fragmentList.add(D4pFragment())
+
+        // Set the first fragment
+        bnv = findViewById(R.id.darBottomNavigationView)
+        supportFragmentManager.beginTransaction().replace(R.id.darflFragment, fragmentList[0])
+            .commit()
+
+        // Set the listener for the BottomNavigationView
+        bnv?.setOnItemReselectedListener { item ->
+            when (item.itemId) {
+                R.id.da1rshan -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.darflFragment, fragmentList[0]).commit()
+                }
+                R.id.pr2ajapati -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.darflFragment, fragmentList[1]).commit()
+                }
+                R.id.n013584247 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.darflFragment, fragmentList[2]).commit()
+                }
+                R.id.d4p -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.darflFragment, fragmentList[3]).commit()
+                }
+            }
+        }
     }
 }
