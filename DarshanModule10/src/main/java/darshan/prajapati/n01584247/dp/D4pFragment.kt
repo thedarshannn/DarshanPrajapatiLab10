f3153f2ea072b0549e64244a4a671e1d938167c0
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-31 22:09:38 -0400

    Implemented ad with click counter.

--- DarshanModule10/src/main/java/darshan/prajapati/n01584247/dp/D4pFragment.kt
+++ DarshanModule10/src/main/java/darshan/prajapati/n01584247/dp/D4pFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -18,6 +22,8 @@ import java.util.Locale
 class D4pFragment : Fragment() {
 
     private lateinit var dateTimeTextView: TextView
+    private lateinit var adView: AdView
+    private var adClickCount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -30,6 +36,7 @@ class D4pFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dateTimeTextView = view.findViewById(R.id.darDateTimeTV)
+        adView = view.findViewById(R.id.darAdView)
         val dateFormat = SimpleDateFormat("yyyy:MM:dd         hh:mm:ss", Locale.getDefault())
 
         val job = CoroutineScope(Dispatchers.Main).launch {
@@ -40,6 +47,16 @@ class D4pFragment : Fragment() {
                 dateTimeTextView.text = dateTime
                 // Delay for 1 second
                 delay(1000)
+            }
+        }
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+        adView.adListener = object : AdListener() {
+            override fun onAdClicked() {
+                adClickCount++
+                val message = getString(R.string.full_name) + ", ad counter $adClickCount times"
+                Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
             }
         }
 
