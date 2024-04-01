38fee055e4a939cc20b7c3bdb939e4b175fc2d3c
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-31 22:05:24 -0400

    Implemented clock with continuous update.

--- DarshanModule10/src/main/java/darshan/prajapati/n01584247/dp/D4pFragment.kt
+++ DarshanModule10/src/main/java/darshan/prajapati/n01584247/dp/D4pFragment.kt
@@ -1,12 +1,23 @@
 package darshan.prajapati.n01584247.dp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 class D4pFragment : Fragment() {
+
+    private lateinit var dateTimeTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,4 +27,21 @@ class D4pFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_d4p, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dateTimeTextView = view.findViewById(R.id.darDateTimeTV)
+        val dateFormat = SimpleDateFormat("yyyy:MM:dd         hh:mm:ss", Locale.getDefault())
+
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            while (true) {
+                // Get current date and time
+                val dateTime = dateFormat.format(Date())
+                // Update TextView with current date and time
+                dateTimeTextView.text = dateTime
+                // Delay for 1 second
+                delay(1000)
+            }
+        }
+
+    }
 }
